e2797aeba176793765f883432ee04b4c26ddb45b
https://github.com/renatomorenoagudo <renatomorenoagudo2@gmail.com>
2024-12-30 10:42:32 -0300

    Gpt IA

---
+++ AI-Applyin/gptAPI.py
@@ -0,0 +1,23 @@
+#primary, in terminal: pip install openai
+#terminal : setx OPENAI_API_KEY "your_api_key_here"
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        
+        {
+            "role": "user",
+            "content": "Você é um experiente programador !",
+        }
+    ],
+    model="gpt-4o", # ou gpt-03.5-turbo
+    max_tokens= 200,
+    temperature= 0.1 #criatividade
+)
+print(chat_completion.choices[0].message)
+
